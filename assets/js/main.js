81f3181f049e51ba6e788b7d6e1e7af8d7d0760f
santiagor06 <santiago.restrepo06@hotmail.com>
2023-09-19 22:00:17 -0500

    join menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
 const email = document.querySelector(".email");
 const menu = document.querySelector(".draw-menu");
+const iconHamburguer = document.querySelector(".menu");
+const menuMobile = document.querySelector(".menu-mobile");
 email.addEventListener("click", toggleMenu);
+iconHamburguer.addEventListener("click", toggleMenuMobile);
 
 function toggleMenu() {
   menu.classList.toggle("inactive");
+}
+function toggleMenuMobile() {
+  menuMobile.classList.toggle("inactive");
 }
